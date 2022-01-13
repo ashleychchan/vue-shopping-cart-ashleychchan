5ca6f5a0389e879e3f217ec4cd8fb9fdac1b4ecb
darrenh6 <90792417+ashleychchan@users.noreply.github.com>
2022-01-13 13:52:40 -0500

    dd

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -2,17 +2,33 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 v-if="graduated">{{student}}</h1>
+    <h1 v-else>"stupid"</h1>
+<ul>
+  <li v-for="animal in animals" :key="animal">{{animal}}
+  </li>
+</ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: {},
+  data(){
+    return {
+      student:"harry",
+      graduated: false,
+      animals: ["doo","poo","ajfj","sgsa"]
+    };
   }
 }
 </script>
+<style>
+h1{
+  color: red;
+}
+</style>
