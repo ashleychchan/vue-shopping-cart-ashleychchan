b59284f5604e28bef911b038a86f66151b082853
darrenh6 <90792417+ashleychchan@users.noreply.github.com>
2022-01-27 13:59:20 -0500

    d

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <h1 v-if="graduated">{{student}}</h1>
     <h1 v-else>"stupid"</h1>
 <ul>
@@ -28,9 +26,10 @@
 <span>{{ selected }}</span>
 <h2>{{now}}</h2>
 <Button>hi</Button>
-<Card title="HElllo" description="dfgdfg trsggd" :cost="77"
- image="https://cdn.pixabay.com/photo/2021/07/25/12/22/tourist-attraction-6491734_960_720.jpg"/>
- <Card v-for="destination in destination" :key="destination.name"/>
+<!-- <Card title="HElllo" description="dfgdfg trsggd" :cost="77"
+ image="https://cdn.pixabay.com/photo/2021/07/25/12/22/tourist-attraction-6491734_960_720.jpg"/> -->
+ <Card v-for="destination in destinations" :key="destination.name"
+ :title="destination.name" />
   </div>
 </template>
 
@@ -55,14 +54,84 @@ export default {
       selected:"",
       destinations:[
         {
+          name: "insulin",
+          description:"too bad for you if you have diabetes",
+          cost:"88.00",
+          img:"https://cdn.pixabay.com/photo/2017/01/11/20/39/insulin-syringe-1972843_960_720.jpg"
+        },
+        {
+          name: "beer",
+          description:"too bad for you if are alcoholic",
+          cost:"12.00",
+          img:"https://cdn.pixabay.com/photo/2017/01/11/20/39/insulin-syringe-1972843_960_720.jpg"
+        },
+        {
+          name: "perfume",
+          description:"too bad for you if you want to give people asthma attacks",
+          cost:"88.00",
+          img:"https://cdn.pixabay.com/photo/2017/01/11/20/39/insulin-syringe-1972843_960_720.jpg"
+        },
+        {
+          name: "plastic bags",
+          description:"too bad for you if you want to choke turtles",
+          cost:"88.00",
+          img:"https://cdn.pixabay.com/photo/2017/01/11/20/39/insulin-syringe-1972843_960_720.jpg"
+        },
+        {
+          name: "nail polish",
+          description:"too bad for your PLAIN NAILS",
+          cost:"88.00",
+          img:"https://cdn.pixabay.com/photo/2017/01/11/20/39/insulin-syringe-1972843_960_720.jpg"
+        },
+        {
+          name: "crayon",
+          description:"too bad for your inner child",
+          cost:"88.00",
+          img:"https://cdn.pixabay.com/photo/2017/01/11/20/39/insulin-syringe-1972843_960_720.jpg"
+        },
+        {
+          name: "cake mixes",
+          description:"too bad for you if you want fluffy cow cakes",
+          cost:"88.00",
+          img:"https://cdn.pixabay.com/photo/2017/01/11/20/39/insulin-syringe-1972843_960_720.jpg"
+        },
+        {
+          name: "bagels",
+          description:"too bad for you if you like cream cheese",
+          cost:"88.00",
+          img:"https://cdn.pixabay.com/photo/2017/01/11/20/39/insulin-syringe-1972843_960_720.jpg"
+        },
+        {
+          name: "cigarettes",
+          description:"Too bad for you if you want to be cool",
+          cost:"88.00",
+          img:"https://cdn.pixabay.com/photo/2017/01/11/20/39/insulin-syringe-1972843_960_720.jpg"
+        },
+        {
+          name: "toothpaste",
+          description:"Too bad for you if you want clean teeth",
+          cost:"88.00",
+          img:"https://cdn.pixabay.com/photo/2017/01/11/20/39/insulin-syringe-1972843_960_720.jpg"
+        },
+        {
+          name: "gummy candies",
+          description:"Too bad for you want to live life YOLO",
+          cost:"88.00",
+          img:"https://cdn.pixabay.com/photo/2017/01/11/20/39/insulin-syringe-1972843_960_720.jpg"
+        },
+        {
+          name: "hearts xenotransplantation",
+          description:"Too bad for you if you have a broken heart",
+          cost:"88.00",
+          img:"https://cdn.pixabay.com/photo/2017/01/11/20/39/insulin-syringe-1972843_960_720.jpg"
+        },
+
+        {
           name: "Insulin",
           description:"Too bad for you if you have diabetes",
           cost:"88.00",
           img:"https://cdn.pixabay.com/photo/2017/01/11/20/39/insulin-syringe-1972843_960_720.jpg"
         },
-        {
-          name:"omg"
-        }
       ]
     };
   },
