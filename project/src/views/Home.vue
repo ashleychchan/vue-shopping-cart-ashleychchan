18cc8b430db9645536193893d20760e5d44ed993
ashash <90792417+ashleychchan@users.noreply.github.com>
2022-02-07 20:53:42 -0500

    d

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -74,61 +74,61 @@ export default {
         {
           name: "perfume",
           description:"too bad for you if you want to give people asthma attacks",
-          cost:"88.00",
+          cost:"97.00",
           img:"https://m.media-amazon.com/images/I/41VIkN0BOML._SL1037_.jpg"
         },
         {
           name: "plastic bag",
           description:"too bad for you if you want to choke turtles",
-          cost:"88.00",
+          cost:"1 turtlelife ",
           img:"https://www.popsci.com/uploads/2020/10/28/5BTIYSAMBJFPNBSWOHYQW4KKBE.jpg?auto=webp&width=1440&height=1080"
         },
         {
           name: "nail polish",
           description:"too bad for your PLAIN NAILS",
-          cost:"88.00",
+          cost:"15.00",
           img:"https://cdn.shopify.com/s/files/1/2665/7478/products/1-PDP-AW-Bottle-Hand.png?v=1603778658"
         },
         {
           name: "crayon",
           description:"too bad for your inner child",
-          cost:"88.00",
+          cost:"7.00",
           img:"https://shop.crayola.com/dw/image/v2/AALB_PRD/on/demandware.static/-/Sites-crayola-storefront/default/dwdfa66042/images/52-3008_8ct_Crayons_PDP_02.jpg?sw=790&sh=790&sm=fit&sfrm=jpg"
         },
         {
           name: "cake mix",
           description:"too bad for you if you want fluffy cakes",
-          cost:"88.00",
+          cost:"8.00",
           img:"https://i5.walmartimages.com/asr/8b383ecf-736c-46ea-8758-2482639c1180.282e759491d1beeb1a497b4fc52ad75e.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
         },
         {
           name: "bagel",
           description:"too bad for you if you like cream cheese",
-          cost:"88.00",
+          cost:"2.00",
           img:"https://cdn.loveandlemons.com/wp-content/uploads/2020/05/bagel-recipe.jpg"
         },
         {
           name: "cigarettes",
           description:"Too bad for you if you want to be cool",
-          cost:"88.00",
+          cost:"25.00",
           img:"https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_17/3468637/210428-cigarettes-jm-1128.jpg"
         },
         {
           name: "toothpaste",
           description:"Too bad for you if you want clean teeth",
-          cost:"88.00",
+          cost:"11.00",
           img:"https://www.cvs.com/bizcontent/merchandising/productimages/large/35000971593_4.jpg"
         },
         {
           name: "gummy candies",
           description:"Too bad for you want to live life YOLO",
-          cost:"88.00",
+          cost:"3.00",
           img:"https://upload.wikimedia.org/wikipedia/commons/a/a6/Oursons_g%C3%A9latine_march%C3%A9_Rouffignac.jpg"
         },
         {
           name: "hearts for xenotransplantation",
           description:"Too bad for you if you have a broken heart",
-          cost:"88.00",
+          cost:"3300.00",
           img:"https://www.news-medical.net/image.axd?picture=2018%2F12%2Fshutterstock_369086330.jpg"
         },
 
